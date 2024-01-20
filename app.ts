d731c79949ab68ecb570e59cb82a664c846e9cad
MDCDEV <me@mdcdev.me>
2024-01-20 00:29:25 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,5 +1,3 @@
-
-
 import { Application } from "https://deno.land/x/oak@v12.6.2/mod.ts";
 import { default as APIV1Router } from "./src/routes/v1.ts";
 import { default as AuthRouter } from "./src/routes/auth.ts";
@@ -10,11 +8,9 @@ const app = new Application();
 const HOST = Deno.env.get("HOST") || "http://localhost";
 const PORT = Number(Deno.env.get("PORT")) || 4000;
 
-if(!Deno.env.get("DONTCRASHPLEASE")) {
-    app.use(APIV1Router.routes());
-    app.use(AuthRouter.routes());
-    app.use(notFound);
-}
+app.use(APIV1Router.routes());
+app.use(AuthRouter.routes());
+app.use(notFound);
 
 console.log(`Server is started at ${HOST}:${PORT}`);
 await app.listen({ port: PORT });
